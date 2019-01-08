6af8bddf414b5cdfd5a02d21ff766b1fc06a341d
Humberto Pinheiro <hpinheiro@avenuecode.com>
2019-01-08 16:24:34 -0200

    Find Todo by id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,17 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {Todo} = require('./../models/Todo');
 const {app} = require('./../server');
 
 const todos = [
     {
+        _id: new ObjectID(),
         text: "First todo"
     },
     {
+        _id: new ObjectID(),
         text: "Second todo"
     }
 ];
@@ -66,5 +69,31 @@ describe('GET /todos', () => {
                 expect(res.body.todos.length).toBe(2);
             })
             .end(done);
+    });
+});
+
+describe('GET /todos/id', () => {
+    it('Should return a todo', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            }).end(done);
+    });
+
+    it('Should return 404 if todo not found', (done) => {
+        const _id = new ObjectID();
+        request(app)
+            .get(`/todos/${_id.toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+    it('Should return 404 for non-object ids', (done) => {
+        request(app)
+            .get(`/todos/123`)
+            .expect(404)
+            .end(done);
+
     });
 });
